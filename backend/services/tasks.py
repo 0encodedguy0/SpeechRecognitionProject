8955014bc227109adf290b378e213ad48030343a
Alexander <alexander.skachihin@gmail.com>
2024-12-14 15:08:24 +0300

    Добавлены асинхронные задачи Celery

--- backend/services/tasks.py
+++ backend/services/tasks.py
@@ -1,12 +1,15 @@
-from celery_worker import celery_app
-from  services.speech_to_text import process_audio_to_text
-from servicec.text_to_speech import process_text_to_audio
+from celery import Celery
+from backend.services.speech_to_text import process_audio_to_text
+from backend.services.text_to_speech import generate_audio_from_text
+
+celery_app = Celery("tasks", broker="redis://localhost:6379/0")
 
 @celery_app.task
-def speech_to_text_task(file_path: str) -> str:
-    with open(file_path, "rb") as file:
-        return process_audio_to_text(file)
+def async_speech_to_text(file_path: str):
+    return process_audio_to_text(file_path)
 
 @celery_app.task
-def text_to_speech_task(text: str) -> str:
-    return process_text_to_audio(text)
+def async_text_to_speech(text: str):
+    output_path = f"temp/output_async.wav"
+    generate_audio_from_text(text, output_path)
+    return output_path
